df43f85cde72f97857a7477f94a09fe277e7a5dc
Viacheslav <surzhenkoviacheslav@gmail.com>
2023-10-24 17:27:27 +0300

    App

---
+++ src/components/FaqSection.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { LayoutGroup } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
+
+const FaqSection = () => {
+  const [element, controls] = useScroll();
+  return (
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <LayoutGroup>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </Toggle>
+        <Toggle title="What product do you offer">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </Toggle>
+      </LayoutGroup>
+    </Faq>
+  );
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+  @media (max-width: 1300px) {
+    display: block;
+    padding: 2rem 2rem;
+    text-align: center;
+  }
+`;
+
+export default FaqSection;
